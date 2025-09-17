f3944a0b2f988dccfeb14a6472d85a042ce0e932
nekennav <neilmadrigal3@gmail.com>
2025-09-17 10:34:05 +0800

    Update qmain.py

--- qmain.py
+++ qmain.py
@@ -29,7 +29,7 @@ st.markdown("""
     background-position: center;
     background-repeat: no-repeat;
     background-color: #000000; /* Fallback color to prevent white blanks */
-    animation: panningBackground 40s linear infinite;
+    animation: panningBackground 50s linear infinite;
     color: #FFFFFF;
 }
 @keyframes panningBackground {
@@ -807,5 +807,6 @@ elif st.session_state.page == "PREDICTIVE MERGER":
             )
         except Exception as e:
             st.error(f"Error creating merged file: {str(e)}")
+
 
 
